1f724748f187c0a33040a4f1d54afd2364b1e897
Simon Meers <simon@simonmeers.com>
2025-08-31 20:09:27 +1000

    add color stubs

---
+++ spike_stubs/color.pyi
@@ -0,0 +1,15 @@
+
+"""Named color constants as integers (per SPIKE Python help)."""
+from typing import Final
+BLACK: Final[int] = 0
+MAGENTA: Final[int] = 1
+PURPLE: Final[int] = 2
+BLUE: Final[int] = 3
+AZURE: Final[int] = 4
+TURQUOISE: Final[int] = 5
+GREEN: Final[int] = 6
+YELLOW: Final[int] = 7
+ORANGE: Final[int] = 8
+RED: Final[int] = 9
+WHITE: Final[int] = 10
+UNKNOWN: Final[int] = -1
